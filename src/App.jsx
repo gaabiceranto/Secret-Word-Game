dc7cc139c6c95391926720c001ab3f83003cfd35
Gabriela Ceranto <gabiceranto@gmail.com>
2023-02-18 16:21:57 -0300

    Criando Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+// react
+import { useState,useEffect,useCallback } from 'react'
 import reactLogo from './assets/react.svg'
+
+// css
 import './App.css'
 
+// data
+import { wordsList } from './data/words'
+
+//components
+import StartScreen from './components/StartScreen'
+import Game from './components/Game'
+import GameOver from './components/GameOver'
+
+
+
+const stages = [
+  {id:1, name:"start"},
+  {id:2, name:"game"},
+  {id:1, name:"end"}
+];
+
 function App() {
+  const [gameStage,setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
 
   return (
     <div className="App">
-      
+      {gameStage == "start" && <StartScreen/>}
+      {gameStage == "game" && <Game/>}
+      {gameStage == "over" && <GameOver/>}
     </div>
   )
 }
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function StartScreen() {
+  return (
+    <>
+        <h1>Secret Word</h1>
+        <p>Clique no botão para jogar</p>
+        <button>Start Game</button>
+    </>
+  )
+}
