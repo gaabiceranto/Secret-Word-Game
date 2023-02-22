ed48a8a45ca6de16b64a97d0e910a27d2a492b1d
Gabriela Ceranto <gabiceranto@gmail.com>
2023-02-22 14:52:09 -0300

    Css primário da tela do jogo

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import "./game.css"
 
 export default function Game({verifyLetter}) {
   return (
     <>
         <div>Game</div>
+        <p>
+          <span>Pontação: 000</span>
+        </p>
+        <h1>Adivinhe a palavra:</h1>
+        <h3>
+          Dica sobre a palavra: <span>Dica...</span>
+        </h3>
+        <div className='wordContainer'>
+          <span className='letter'>A</span>
+          <span className='blankSquare'></span>
+        </div>
+        <div>
+          <p>Tente Adivinhar uma letra da palavra:</p>
+          <form>
+            <input type="text" name="letter" maxLength="1" required/>
+          </form>
+        </div>
+        <div className="wrongLettersContainer">
+          <p>Letras já utilizadas:</p>
+          <span>a,</span>
+          <span>b,</span>
+        </div>
         <button onClick={verifyLetter}>Finalzia jogo</button>
     </>
   )
